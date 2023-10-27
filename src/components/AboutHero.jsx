ca4d100b7779b7cd466a77b3b87f31d4607fb0d3
DevaWinner <winneraaniekan@gmail.com>
2023-10-28 00:14:13 +0100

    Feature: Add about hero section with styling

---
+++ src/components/AboutHero.jsx
@@ -0,0 +1,34 @@
+import { Container, Col, Row } from "react-bootstrap";
+import "../styles/AboutHero.css";
+
+const AboutHero = () => {
+	return (
+		<>
+			<section className="about-hero">
+				<img src="/assets/about-hero-scaled.jpg" alt="" className="img-fluid" />
+				<div className="about-hero-text">
+					<h1>About Inclusion Africa</h1>
+					<p>
+						Inclusion Afrika was incorporated in Accra Ghana by Joseph Bentum
+						and David Rupper in June 2023. Although Inclusion Afrika is based in
+						Ghana, it has a network throughout Sub-Saharan Africa. Inclusion
+						Afrika was organized because African young adults are facing many
+						challenges including poverty, inadequate education, unemployment and
+						a lack of capital or opportunity.
+					</p>
+				</div>
+			</section>
+			<Container>
+				<div className="about-quote d-flex flex-column container">
+					<blockquote>
+						“Inclusion is the act of including someone or something as part of a
+						group...”
+					</blockquote>
+					<p>-Cambridge Dictionary definition of “inclusion”</p>
+				</div>
+			</Container>
+		</>
+	);
+};
+
+export default AboutHero;
