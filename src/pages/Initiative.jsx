11895a6c4070607413c9a7f3eb107f40cd9677b0
DevaWinner <winneraaniekan@gmail.com>
2023-11-01 01:07:26 +0100

    Mount Innnitiative page

--- src/pages/Initiative.jsx
+++ src/pages/Initiative.jsx
@@ -1,8 +1,45 @@
+import ProgramAreas from "../components/ProgramAreas";
+import CtaBox from "../components/CtaBox";
+import Testimonial from "../components/Testimonial";
+
 function Initiative() {
 	return (
-		<div>
-			<h1>Initiative Page</h1>
-		</div>
+		<>
+			<div className="team-header">
+				<img
+					src="/assets/medium-shot-happy-community.jpg"
+					alt="medium-shot-happy-african-people"
+					className="img-fluid"
+				/>
+				<div className="overlay">
+					<h1 className="text">Our Initiatives</h1>
+				</div>
+			</div>
+			<section className="container top">
+				<div className="text d-flex">
+					<div className="title d-flex"></div>
+					<p>
+						Our initiatives are designed to make a tangible difference in the
+						lives of African youth. With a commitment to collaboration and
+						inclusion, we have crafted a diverse range of programs that address
+						these challenges head-on..
+					</p>
+				</div>
+				<div className="text d-flex">
+					<div className="title d-flex">
+						<h2>Program Areas</h2>
+						<hr />
+					</div>
+					<p>
+						Explore our initiatives below to see how we're working towards a
+						brighter future for African young adults:
+					</p>
+				</div>
+			</section>
+			<ProgramAreas />
+			<CtaBox />
+			<Testimonial />
+		</>
 	);
 }
 
