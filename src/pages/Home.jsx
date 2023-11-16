0e98ac1ad5b26b053466abcaafbc803f4ef985d2
DevaWinner <winneraaniekan@gmail.com>
2023-11-16 12:17:03 +0100

    Update Home.jsx with motion components and ContactForm2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,37 @@ import Reach from "../components/Reach";
 import WhoWeAreSection from "../components/WhoWeAreSection";
 import WhatWeDo from "../components/WhatWeDo";
 import CtaBox from "../components/CtaBox";
-import ContactForm from "../components/ContactForm";
+import ContactForm from "../components/ContactForm2";
 import PartnerSection from "../components/PartnerSection";
 import "../styles/Home.css";
+import { motion } from "framer-motion";
 
 function Home() {
 	return (
 		<>
-			<div style={{ zIndex: 1 }}>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 1 }}
+			>
 				<Navbar />
 				<section className="hero">
 					<HeroCarousel />
 
 					<div className="overlay heading">
-						<div className="text">
+						<motion.div
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0  }}
+              transition={{ duration: 1.2, delay: 1 }}
+							className="text"
+						>
 							<h1>
 								Become a
 								<br />
 								Catalyst for <br />
 								Good in Africa
 							</h1>
-						</div>
+						</motion.div>
 					</div>
 				</section>
 				<Reach />
@@ -33,7 +43,7 @@ function Home() {
 				<CtaBox />
 				<ContactForm />
 				<PartnerSection />
-			</div>
+			</motion.div>
 		</>
 	);
 }
